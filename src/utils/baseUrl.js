e982533ca6c483d528b4e2122ff6e7c9079a48ee
nikhilkandari77 <nikhilkandari77@gmail.com>
2023-09-12 16:00:00 +0530

    Merge branch 'master' of https://github.com/nikhilkandari77/tricareservices_react2
    # Conflicts:
    #	build/asset-manifest.json
    #	build/index.html
    #	package-lock.json
    #	package.json
    #	src/pages/Customerdetail.js
    #	src/pages/UserPage.js
    #	src/sections/@dashboard/products/ProductTable.js
    #	src/utils/baseUrl.js
    #	yarn.lock

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -3,7 +3,8 @@ import React from "react";
 
 //  const baseUrl="https://tricareservice-y2ee.onrender.com";
 //  const baseUrl="https://tricareservice.onrender.com";
- const baseUrl="http://localhost:10000"
+// const baseUrl="http://localhost:10000"
 // const baseUrl="http://192.168.29.54:8082"
+    const baseUrl="http://52.66.197.147:9001"
 
 export default baseUrl;
