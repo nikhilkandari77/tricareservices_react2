a04ad0600b5562651acaede7f6eafebb8f7a8be7
nikhilkandari77 <nikhilkandari77@gmail.com>
2023-10-19 12:08:46 +0530

    Merge branch 'master' of https://github.com/nikhilkandari77/tricareservices_react2
    # Conflicts:
    #	build/static/js/main.e496646a.js.map
    #	src/pages/Customerdetail.js
    #	src/utils/baseUrl.js
    #	yarn.lock

--- src/sections/@dashboard/products/ProductTable.js
+++ src/sections/@dashboard/products/ProductTable.js
@@ -173,9 +173,10 @@ export default function StickyHeadTable() {
         toast.success('Product added successfully'); // Display success toast
         setRefresh((prevRefresh) => !prevRefresh);
       } else {
-        console.error('Failed to add product');
-        toast.error('Failed to add product'); // Display error toast
-      }
+        const errorMessage = await response.text().then(text=>JSON.parse(text)); // Extract backend error message
+        console.error('Invalid product data format', errorMessage);
+        toast.error(`Invalid product data format: ${errorMessage.message}`);
+    }
     } catch (error) {
       console.error('Error while adding product:', error);
       toast.error('Error while adding product'); // Display error toast
@@ -940,7 +941,6 @@ export default function StickyHeadTable() {
                                                       fullWidth
                                                       required
                                                       multiline
-                                                      inputProps={{ maxLength: 150 }}
                                                       margin="normal" // Added margin for spacing between fields
                                                     /><br /><br />
                                                     <div style={{ textAlign: "center" }}><h5>Select images for delete</h5></div>
