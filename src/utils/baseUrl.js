bc2be2ce153ad372e4879d5a30534b6e375696bd
nikhilkandari77 <nikhilkandari77@gmail.com>
2023-11-02 11:05:26 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/pages/Customerdetail.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-//  const baseUrl="https://tricareservice.onrender.com";
+  const baseUrl="https://tricareservice.onrender.com";
 // const baseUrl="http://3.110.83.143:9001" // AWS
 // const baseUrl="http://192.168.29.54:9001" // C6_4G
-  const baseUrl="http://localhost:9001" // Home Wifi
+//  const baseUrl="http://localhost:9001" // Home Wifi
 export default baseUrl;
